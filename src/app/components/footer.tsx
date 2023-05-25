223a60dc4beb1f32f8505fd4b68fbc71294e4980
Kevstar9-1 <kevstar924@gmail.com>
2023-05-25 15:57:24 -0600

    cambio de correo
    se define otro correo en el pie de pagina

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer: React.FC = () => {
   return (
     <footer className="bg-gray-300 text-gray-600 py-4 text-center">
-      <p>Contact information: your@email.com</p>
+      <p>Contact information: soportlt@gmail.com</p>
       <p>Our social media</p>
       <div className="flex items-center justify-center">
         <img src="./ima/facebook.jpeg" alt="Facebook" className="w-5 h-5" /> 
