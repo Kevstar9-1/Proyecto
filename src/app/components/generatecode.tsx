97690338812110e3ded0f5f254535c0b5c91791e
Kevstar9-1 <kevstar924@gmail.com>
2023-06-28 11:10:35 -0600

    creacion de  generatecode.tsx
    creacion de  generatecode.tsx

---
+++ src/app/components/generatecode.tsx
@@ -0,0 +1,10 @@
+
+export function generateTrackingCode(): string {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let code = '';
+  for (let i = 0; i < 7; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    code += characters.charAt(randomIndex);
+  }
+  return code;
+}
